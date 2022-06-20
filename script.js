f0291f76bb91e02b06109dbb568ace816c917f49
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2022-06-20 09:47:45 +0100

    get quotes from API

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const url = 'https://type.fit/api/quotes'
+// SELECTORS
+const quote = document.getElementById('quote')
+const author = document.getElementById('author')
 
-async function getQuote() {
-    const response = await fetch(url)
-    const quoteArray = await response.json()
-    console.log(quoteArray)
+//make the variable global
+let quoteArray = [1]
+
+
+//EVENT LISTENERS
+
+
+
+//FUNCTIONS
+//get random number
+
+async function getQuoteArray() {
+    // get array of quotes from API
+    const url = 'https://type.fit/api/quotes'
+    try {
+        const response = await fetch(url)
+        quoteArray = await response.json()
+        getQuote()
+    }
+
+    catch(error) {
+        console.log('That didnt work')
+    }
 }
 
-getQuote()
+function getQuote() {
+    const index = Math.floor(Math.random() * quoteArray.length)
+    const randomQuote = quoteArray[index]
+    console.log(randomQuote)
+}
+
+getQuoteArray()
+
+
+
+
+
